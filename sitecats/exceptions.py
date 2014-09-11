5fd9b102bcaf1b7a4155665cac05801aebae1a89
idle sign <idlesign@yandex.ru>
2014-09-11 14:30:20 +0700

    Added new exception type.

--- sitecats/exceptions.py
+++ sitecats/exceptions.py
@@ -1,13 +1,16 @@
+from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
+from django.forms.fields import ValidationError
+
 
 class SitecatsException(Exception):
     """"""
 
 
-class SitecatsConfigurationError(SitecatsException):
+class SitecatsConfigurationError(SitecatsException, ImproperlyConfigured):
     """"""
 
 
-class SitecatsSecurityException(SitecatsException):
+class SitecatsSecurityException(SitecatsException, SuspiciousOperation):
     """"""
 
 
@@ -16,5 +19,9 @@ class SitecatsNewCategoryException(SitecatsSecurityException):
 
 
 class SitecatsLockedCategoryDelete(SitecatsException):
+    """"""
+
+
+class SitecatsValidationError(SitecatsException, ValidationError):
     """"""
 
