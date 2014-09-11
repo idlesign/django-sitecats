c06db46e02425f4f9156b1f98e1d289fc25f459f
idle sign <idlesign@yandex.ru>
2014-09-11 22:32:57 +0700

    `sitecats_categories` template tag made more intelligent.

--- sitecats/templatetags/sitecats.py
+++ sitecats/templatetags/sitecats.py
@@ -1,26 +1,15 @@
 from django import template
+from django.conf import settings
 
+from ..models import ModelWithCategory
+from ..toolbox import CategoryRequestHandler, CategoryList
+from ..exceptions import SitecatsConfigurationError
 
 register = template.Library()
 
 
 @register.tag
 def sitecats_categories(parser, token):
-    return get_generic_node(parser, token,
-           '`sitecats_categories` tag expects the following notation: {% sitecats_categories from my_categories_list template "sitecats/my_categories.html" %}.',
-           'sitecats_categories', 'sitecats/categories.html')
-
-
-def get_generic_node(parser, token, error_msg, context_var, default_template):
-    """Returns a generic node object for a template tag.
-
-    :param Parser parser:
-    :param Token token:
-    :param str error_msg:
-    :param str context_var:
-    :param str default_template:
-    :return:
-    """
     tokens = token.split_contents()
     use_template = detect_clause(parser, 'template', tokens)
     target_obj = detect_clause(parser, 'from', tokens)
@@ -28,28 +17,38 @@ def get_generic_node(parser, token, error_msg, context_var, default_template):
     tokens_num = len(tokens)
 
     if tokens_num in (1, 3):
-        return sitecats_genericNode(target_obj, use_template, context_var, default_template)
+        return sitecats_categoriesNode(target_obj, use_template)
     else:
-        raise template.TemplateSyntaxError(error_msg)
+        raise template.TemplateSyntaxError('`sitecats_categories` tag expects the following notation: {% sitecats_categories from my_categories_list template "sitecats/my_categories.html" %}.')
 
 
-class sitecats_genericNode(template.Node):
+class sitecats_categoriesNode(template.Node):
 
-    def __init__(self, target_obj, use_template, context_var, default_template):
+    def __init__(self, target_obj, use_template):
         self.use_template = use_template
         self.target_obj = target_obj
-        self.context_var = context_var
-        self.default_template = default_template
 
     def render(self, context):
         resolve = lambda arg: arg.resolve(context) if isinstance(arg, template.FilterExpression) else arg
 
-        context.push()
         target_obj = resolve(self.target_obj)
-        if not isinstance(target_obj, (list, tuple)):
-            target_obj = (target_obj,)
-        context[self.context_var] = target_obj
-        contents = template.loader.get_template(resolve(self.use_template or self.default_template)).render(context)
+        if isinstance(target_obj, CategoryRequestHandler):
+            target_obj = target_obj.get_lists()
+        elif isinstance(target_obj, ModelWithCategory):
+            target_obj = target_obj.get_category_lists()
+        elif isinstance(target_obj, (list, tuple)):  # Simple list of CategotyList items.
+            pass
+        else:
+            if isinstance(target_obj, CategoryList):
+                target_obj = (target_obj,)
+            else:
+                if settings.DEBUG:
+                    raise SitecatsConfigurationError('`sitecats_categories` template tag can\'t accept `%s` type from `%s` template variable.' % (type(target_obj), self.target_obj))
+                return ''  # Silent fall.
+
+        context.push()
+        context['sitecats_categories'] = target_obj
+        contents = template.loader.get_template(resolve(self.use_template or 'sitecats/categories.html')).render(context)
         context.pop()
 
         return contents
