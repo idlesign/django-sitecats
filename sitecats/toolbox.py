47eb4a5eade7cacb1d9a6d43cfbfb303a9fdad67
idle sign <idlesign@yandex.ru>
2014-09-11 14:46:57 +0700

    Implemented min_num and max_num checks for category editor.

--- sitecats/toolbox.py
+++ sitecats/toolbox.py
@@ -1,12 +1,12 @@
 from inspect import getargspec
 from collections import namedtuple, OrderedDict
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ungettext_lazy as _n
 from django.utils.six import string_types
 from django.contrib import messages
 
 from .utils import get_category_model, Cache
-from .exceptions import SitecatsConfigurationError, SitecatsSecurityException, SitecatsNewCategoryException
+from .exceptions import SitecatsConfigurationError, SitecatsSecurityException, SitecatsNewCategoryException, SitecatsValidationError
 
 
 sitecats_cache = Cache()
@@ -80,11 +80,12 @@ class CategoryRequestHandler(object):
 
             self._lists[lst.get_id()] = lst
 
-    def action_remove(self, category_list):
+    @classmethod
+    def action_remove(cls, request, category_list):
         if not category_list.editor.allow_remove:
             raise SitecatsSecurityException('`action_remove()` is not supported by parent `%s`category.' % category_list.alias)
 
-        category_id = int(self._request.POST.get('category_id', 0))
+        category_id = int(request.POST.get('category_id', 0))
         if not category_id:
             raise SitecatsSecurityException('Unsupported `category_id` value - `%s` - is passed to `action_remove()`.' % category_id)
 
@@ -100,21 +101,29 @@ class CategoryRequestHandler(object):
         if category.parent_id != category_list.get_id():
             raise SitecatsSecurityException('`action_remove()` is unable to remove `%s`: not a child of parent `%s` category.' % (cat_ident, category_list.alias))
 
-        # TODO min
-        # TODO remove for certain user / for all
-        # TODO remove with certain status
+        min_num = category_list.editor.min_num
+
+        def check_min_num(num):
+            if min_num is not None and num-1 < min_num:
+                raise SitecatsValidationError(_('Unable to remove "%s" category from "%s": parent category requires at least %s %s.') % (category.title, category_list.get_title(), min_num, _n('subcategory', 'subcategories', min_num)))
+
+        child_ids = sitecats_cache.get_child_ids(category_list.alias)
+        check_min_num(len(child_ids))
         if category_list.obj is None:  # Remove category itself and children.
             category.delete()
         else:  # Remove just a category-to-object tie.
+            # TODO filter user/status
+            check_min_num(category_list.obj.get_ties_for_categories_qs(child_ids).count())
             category_list.obj.remove_from_category(category)
 
         return True
 
-    def action_add(self, category_list):
+    @classmethod
+    def action_add(cls, request, category_list):
         if not category_list.editor.allow_add:
             raise SitecatsSecurityException('`action_add()` is not supported by `%s` category.' % category_list.alias)
 
-        category_title = self._request.POST.get('category_title', '').strip()
+        category_title = request.POST.get('category_title', '').strip()
         if not category_title:
             raise SitecatsSecurityException('Unsupported `category_title` value - `%s` - is passed to `action_add()`.' % category_title)
 
@@ -124,17 +133,27 @@ class CategoryRequestHandler(object):
             return exists
 
         if not exists and not category_list.editor.allow_new:
-            raise SitecatsNewCategoryException(_('Unable to create "%s" category inside of "%s": parent category does not support this action.') % (category_title, category_list.get_title()))
+            raise SitecatsNewCategoryException(_('Unable to create a new "%s" category inside of "%s": parent category does not support this action.') % (category_title, category_list.get_title()))
 
+        max_num = category_list.editor.max_num
+
+        def check_max_num(num):
+            if max_num is not None and num+1 > max_num:
+                raise SitecatsValidationError(_('Unable to add "%s" category into "%s": parent category can have at most %s %s.') % (category_title, category_list.get_title(), max_num, _n('subcategory', 'subcategories', max_num)))
+
+        child_ids = sitecats_cache.get_child_ids(category_list.alias)
         if not exists:  # Add new category.
-            target_category = get_category_model().add(category_title, self._request.user, parent=category_list.get_category_model())
+            if category_list.obj is None:
+                check_max_num(len(child_ids))
+            # TODO status
+            target_category = get_category_model().add(category_title, request.user, parent=category_list.get_category_model())
         else:
             target_category = exists  # Use existing one for a tie.
 
-        # TODO max
-        # TODO status
         if category_list.obj is not None:
-            category_list.obj.add_to_category(target_category, self._request.user)
+            # TODO status
+            check_max_num(category_list.obj.get_ties_for_categories_qs(child_ids).count())
+            category_list.obj.add_to_category(target_category, request.user)
 
         return target_category
 
@@ -162,9 +181,12 @@ class CategoryRequestHandler(object):
         action_method = getattr(self, 'action_%s' % requested_action)
 
         try:
-            return action_method(category_list)
+            return action_method(self._request, category_list)
         except SitecatsNewCategoryException as e:
             messages.error(self._request, e)
+            return None
+        except SitecatsValidationError as e:
+            messages.error(self._request, e.messages[0])
             return None
 
     def get_lists(self):
