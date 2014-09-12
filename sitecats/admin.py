855afc87a345cc35f96ad9be1c751e974c23bad5
idle sign <idlesign@yandex.ru>
2014-09-12 22:00:08 +0700

    Implemented locked category restrictions for Django Admin.

--- sitecats/admin.py
+++ sitecats/admin.py
@@ -10,13 +10,26 @@ except ImportError:
     MODEL_ADMIN = admin.ModelAdmin
 
 
-#TODO lock delete and alias edit for is_locked.
 class CategoryAdmin(MODEL_ADMIN):
     list_display = ('title', 'alias', 'is_locked', 'status')
     search_fields = ('title', 'alias', 'note', 'creator')
     list_filter = ('time_created', 'status')
+    ordering = ('sort_order',)
     date_hierarchy = 'time_created'
     hierarchy = True
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and obj.is_locked:
+            return False
+        return super(CategoryAdmin, self).has_delete_permission(request, obj=obj)
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is not None:
+            if obj.is_locked:
+                self.readonly_fields = ('is_locked', 'alias')
+            else:
+                self.readonly_fields = ()
+        return super(CategoryAdmin, self).get_form(request, obj=obj, **kwargs)
 
 admin.site.register(get_category_model(), CategoryAdmin)
 
